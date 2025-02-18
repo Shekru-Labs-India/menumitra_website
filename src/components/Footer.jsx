f0b3780c8506e905d0cb198aebcd2ae89ac83061
Sugatraj <sugatraj.2106@gmail.com>
2025-02-18 16:15:44 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,145 +8,79 @@ const Footer = () => {
     <>
       <section className="footer-top-area bg-white border border-top shadow-lg">
         <div className="container">
-          {/* Top Section with Logo, Contact, Social Icons */}
-          <div className="footer-info-wrapper mb-4">
-            {/* Logo and Name */}
-            <div className="d-flex align-items-center">
-              <Link to="/">
-                <img
-                  src={menumitra}
-                  alt="MenuMitra-Logo"
-                  style={{ width: "50px", height: "50px" }}
-                />
-              </Link>
-              <Link className="ms-3" to="/website">
-                <span className="fs-3 navbar-brand text-dark">MenuMitra</span>
-              </Link>
-            </div>
-
-            {/* Contact Info */}
-            <div className="d-flex align-items-center">
-              <i className="fas fa-mobile-alt me-2" />
-              <span>09172530151</span>
-            </div>
-
-            <div className="d-flex align-items-center">
-              <i className="fa fa-envelope me-2" />
-              <span>info@menumitra.com</span>
-            </div>
-
-            {/* Social Icons */}
-            <ul className="social-icon">
-              <li>
-                <Link to="https://www.facebook.com/share/x5wymXr6w7W49vaQ/?mibextid=qi2Omg" target="_blank" rel="noreferrer">
-                  <i className="fa-brands fa-facebook fs-3 border rounded-circle p-2" style={{ color: "#1877F2", borderColor: "#1877F2" }}></i>
-                </Link>
-              </li>
-              <li>
-                <Link to="https://www.linkedin.com/company/102429337/admin/dashboard/" target="_blank" rel="noreferrer">
-                  <i className="fa-brands fa-linkedin fs-3 border rounded-circle p-2" style={{ color: "#0A66C2", borderColor: "#0A66C2" }}></i>
-                </Link>
-              </li>
-              <li>
-                <Link to="https://www.youtube.com/@menumitra" target="_blank" rel="noreferrer">
-                  <i className="fa-brands fa-youtube fs-3 border rounded-circle p-2" style={{ color: "#FF0000", borderColor: "#FF0000" }}></i>
-                </Link>
-              </li>
-              <li>
-                <Link to="https://t.me/MenuMitra" target="_blank" rel="noreferrer">
-                  <i className="fa-brands fa-telegram fs-3 border rounded-circle p-2" style={{ color: "#26A5E4", borderColor: "#26A5E4" }}></i>
-                </Link>
-              </li>
-              <li>
-                <Link to="https://www.instagram.com/menumitra/" target="_blank" rel="noreferrer">
-                  <i className="fa-brands fa-instagram fs-3 border rounded-circle p-2" style={{ color: "#E4405F", borderColor: "#E4405F" }}></i>
-                </Link>
-              </li>
-            </ul>
-
-            {/* Book a Demo Button */}
-            <Link className="btn btn-outline-dark btn-sm px-3 py-2" to="/book_demo">
-              Book a Demo
-            </Link>
-          </div>
-
           {/* Links Sections */}
           <div className="row">
-            {/* Utility Section */}
-            <div className="col-12 col-sm-6 col-lg-3 mb-4">
-              <div className="single-widget text-sm-start">
-                <h3 className="text-info mb-3">Utility</h3>
-                <ul className="links">
-                  <li><Link className="text-dark d-block py-2" to="/"><i className="fa-solid fa-angle-right text-info me-2"></i>Home</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/pricing"><i className="fa-solid fa-angle-right text-info me-2"></i>Pricing</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/career"><i className="fa-solid fa-angle-right text-info me-2"></i>Career</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/support"><i className="fa-solid fa-angle-right text-info me-2"></i>Support</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/about_us"><i className="fa-solid fa-angle-right text-info me-2"></i>About</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/reseller_program"><i className="fa-solid fa-angle-right text-info me-2"></i>Become Partner</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/devices"><i className="fa-solid fa-angle-right text-info me-2"></i>Devices</Link></li>
+            <div className="col-12 col-md-3">
+              <div className="footer-widget">
+                <h3 className="text-primary mb-4">Utility</h3>
+                <ul className="footer-links">
+                  <li><Link to="/"><i className="fa-solid fa-chevron-right me-2"></i>Home</Link></li>
+                  <li><Link to="/pricing"><i className="fa-solid fa-chevron-right me-2"></i>Pricing</Link></li>
+                  <li><Link to="/career"><i className="fa-solid fa-chevron-right me-2"></i>Career</Link></li>
+                  <li><Link to="/support"><i className="fa-solid fa-chevron-right me-2"></i>Support</Link></li>
+                  <li><Link to="/about_us"><i className="fa-solid fa-chevron-right me-2"></i>About</Link></li>
+                  <li><Link to="/reseller_program"><i className="fa-solid fa-chevron-right me-2"></i>Become Partner</Link></li>
+                  <li><Link to="/devices"><i className="fa-solid fa-chevron-right me-2"></i>Devices</Link></li>
                 </ul>
               </div>
             </div>
 
-            {/* Features Section */}
-            <div className="col-12 col-sm-6 col-lg-3 mb-4">
-              <div className="single-widget text-sm-start">
-                <h3 className="text-info mb-3">Features</h3>
-                <ul className="links">
-                  <li><Link className="text-dark d-block py-2" to="/billing"><i className="fa-solid fa-angle-right text-info me-2"></i>Billing</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/inventory"><i className="fa-solid fa-angle-right text-info me-2"></i>Inventory</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/menu"><i className="fa-solid fa-angle-right text-info me-2"></i>Menu</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/onlineorder"><i className="fa-solid fa-angle-right text-info me-2"></i>Online Order</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/reports"><i className="fa-solid fa-angle-right text-info me-2"></i>Restaurant Reports</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/customermanagement"><i className="fa-solid fa-angle-right text-info me-2"></i>Customer Management</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/customerfeedback"><i className="fa-solid fa-angle-right text-info me-2"></i>Customer Feedback</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/scan"><i className="fa-solid fa-angle-right text-info me-2"></i>Scan & Order</Link></li>
+            <div className="col-12 col-md-3">
+              <div className="footer-widget">
+                <h3 className="text-primary mb-4">Features</h3>
+                <ul className="footer-links">
+                  <li><Link to="/billing"><i className="fa-solid fa-chevron-right me-2"></i>Billing</Link></li>
+                  <li><Link to="/inventory"><i className="fa-solid fa-chevron-right me-2"></i>Inventory</Link></li>
+                  <li><Link to="/menu"><i className="fa-solid fa-chevron-right me-2"></i>Menu</Link></li>
+                  <li><Link to="/onlineorder"><i className="fa-solid fa-chevron-right me-2"></i>Online Order</Link></li>
+                  <li><Link to="/reports"><i className="fa-solid fa-chevron-right me-2"></i>Restaurant Reports</Link></li>
+                  <li><Link to="/customermanagement"><i className="fa-solid fa-chevron-right me-2"></i>Customer Management</Link></li>
+                  <li><Link to="/customerfeedback"><i className="fa-solid fa-chevron-right me-2"></i>Customer Feedback</Link></li>
+                  <li><Link to="/scan"><i className="fa-solid fa-chevron-right me-2"></i>Scan & Order</Link></li>
                 </ul>
               </div>
             </div>
 
-            {/* Customer Section */}
-            <div className="col-12 col-sm-6 col-lg-3 mb-4">
-              <div className="single-widget text-sm-start">
-                <h3 className="text-info mb-3">Customer</h3>
-                <ul className="links">
-                  <li><Link className="text-dark d-block py-2" to="/book_demo"><i className="fa-solid fa-angle-right text-info me-2"></i>Book A Demo</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/help_support"><i className="fa-solid fa-angle-right text-info me-2"></i>Help and support</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/order_tracking"><i className="fa-solid fa-angle-right text-info me-2"></i>Order Tracking</Link></li>
-                  <li><a className="text-dark d-block py-2" href="https://user.menumitra.com/user_app/Index" target="_blank" rel="noreferrer"><i className="fa-solid fa-angle-right text-info me-2"></i>UserApp</a></li>
+            <div className="col-12 col-md-3">
+              <div className="footer-widget">
+                <h3 className="text-primary mb-4">Customer</h3>
+                <ul className="footer-links">
+                  <li><Link to="/book_demo"><i className="fa-solid fa-chevron-right me-2"></i>Book A Demo</Link></li>
+                  <li><Link to="/help_support"><i className="fa-solid fa-chevron-right me-2"></i>Help and support</Link></li>
+                  <li><Link to="/order_tracking"><i className="fa-solid fa-chevron-right me-2"></i>Order Tracking</Link></li>
+                  <li><a href="https://user.menumitra.com/user_app/Index" target="_blank" rel="noreferrer"><i className="fa-solid fa-chevron-right me-2"></i>UserApp</a></li>
                 </ul>
               </div>
             </div>
 
-            {/* Outlet Type Section */}
-            <div className="col-12 col-sm-6 col-lg-3 mb-4">
-              <div className="single-widget text-sm-start">
-                <h3 className="text-info mb-3">Outlet Type</h3>
-                <ul className="links">
-                  <li><Link className="text-dark d-block py-2" to="/newrestaurant"><i className="fa-solid fa-angle-right text-info me-2"></i>New Restaurant</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/finedine"><i className="fa-solid fa-angle-right text-info me-2"></i>Fine Dine</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/foodcourts"><i className="fa-solid fa-angle-right text-info me-2"></i>Food Courts</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/pizzeria"><i className="fa-solid fa-angle-right text-info me-2"></i>Pizzeria</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/cafe"><i className="fa-solid fa-angle-right text-info me-2"></i>Cafe</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/qsr"><i className="fa-solid fa-angle-right text-info me-2"></i>QSR</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/bakeries"><i className="fa-solid fa-angle-right text-info me-2"></i>Bakeries</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/icecream"><i className="fa-solid fa-angle-right text-info me-2"></i>Icecream & Desserts</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/cloudkitchens"><i className="fa-solid fa-angle-right text-info me-2"></i>Cloud Kitchens</Link></li>
-                  <li><Link className="text-dark d-block py-2" to="/largechain"><i className="fa-solid fa-angle-right text-info me-2"></i>Large Chain</Link></li>
+            <div className="col-12 col-md-3">
+              <div className="footer-widget">
+                <h3 className="text-primary mb-4">Outlet Type</h3>
+                <ul className="footer-links">
+                  <li><Link to="/newrestaurant"><i className="fa-solid fa-chevron-right me-2"></i>New Restaurant</Link></li>
+                  <li><Link to="/finedine"><i className="fa-solid fa-chevron-right me-2"></i>Fine Dine</Link></li>
+                  <li><Link to="/foodcourts"><i className="fa-solid fa-chevron-right me-2"></i>Food Courts</Link></li>
+                  <li><Link to="/pizzeria"><i className="fa-solid fa-chevron-right me-2"></i>Pizzeria</Link></li>
+                  <li><Link to="/cafe"><i className="fa-solid fa-chevron-right me-2"></i>Cafe</Link></li>
+                  <li><Link to="/qsr"><i className="fa-solid fa-chevron-right me-2"></i>QSR</Link></li>
+                  <li><Link to="/bakeries"><i className="fa-solid fa-chevron-right me-2"></i>Bakeries</Link></li>
+                  <li><Link to="/icecream"><i className="fa-solid fa-chevron-right me-2"></i>Icecream & Desserts</Link></li>
+                  <li><Link to="/cloudkitchens"><i className="fa-solid fa-chevron-right me-2"></i>Cloud Kitchens</Link></li>
+                  <li><Link to="/largechain"><i className="fa-solid fa-chevron-right me-2"></i>Large Chain</Link></li>
                 </ul>
               </div>
             </div>
-          </div>
-
-          {/* Footer Links */}
-          <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-4 mt-4 pt-4 border-top">
-            <Link className="text-secondary" to="/terms_conditions">Terms and Conditions</Link>
-            <Link className="text-secondary" to="/privacy_policy">Privacy Policy</Link>
-            <Link className="text-secondary" to="/cookie_policy">Cookie Policy</Link>
-            <Link className="text-secondary" to="/request_data_removal">Request Data Removal</Link>
           </div>
         </div>
       </section>
+
+      {/* Footer Links */}
+      <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-4 mt-4 pt-4 border-top">
+        <Link className="text-secondary" to="/terms_conditions">Terms and Conditions</Link>
+        <Link className="text-secondary" to="/privacy_policy">Privacy Policy</Link>
+        <Link className="text-secondary" to="/cookie_policy">Cookie Policy</Link>
+        <Link className="text-secondary" to="/request_data_removal">Request Data Removal</Link>
+      </div>
 
       {/* App Downloads Section */}
       <section className="bg-light py-4 border-top shadow-sm">
