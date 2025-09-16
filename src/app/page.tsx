ff592cb5522f34fab33d8299e866e3b60e12885d
Sugatraj <sugatraj.2106@gmail.com>
2025-09-16 13:04:09 +0530

    feat: Comment out BlogSection in Home component for future updates
    - Temporarily disable BlogSection to streamline the Home page layout.
    - Prepare for potential content revisions or enhancements in upcoming commits.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <SolutionSection />
       <CoreFeaturesSection />
       <TestimonialsSection />
-      <BlogSection />
+      {/* <BlogSection /> */}
       <FooterSection />
     </div>
   );
