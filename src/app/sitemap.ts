85e8f9cdf1494437fec6809bc4f21b527b85dc33
Sugatraj <sugatraj.2106@gmail.com>
2025-09-18 14:08:30 +0530

    feat: Update robots.txt and sitemap.ts for improved SEO and content visibility
    - Modify robots.txt to clarify allowed pages for search engines, enhancing SEO practices.
    - Add new outlet type URLs to sitemap.ts, improving search engine indexing and user navigation.
    - Ensure consistent last modified dates and change frequencies for better crawling efficiency.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -193,7 +193,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/outlet-type/Restaurant`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/outlet-type/cafe`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/outlet-type/Cafe`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
